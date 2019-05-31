279a9e16beafd65fef34b59bdb0fce78d689225f
ozeki <hhdfgg@users.noreply.github.com>
2019-05-31 16:19:00 +0900

    たくさん追加

--- hukusyu0531/Form1.Designer.cs
+++ hukusyu0531/Form1.Designer.cs
@@ -31,6 +31,8 @@
             this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -49,14 +51,40 @@
             this.timer1.Enabled = true;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(388, 190);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 27);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
             this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -66,6 +94,8 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- hukusyu0531/Form1.cs
+++ hukusyu0531/Form1.cs
@@ -14,10 +14,14 @@ namespace hukusyu0531
     {
         int vx = -10;
         int vy = -10;
+        int lcr = 255, lcg = 0, lcb = 0;//label1の文字の色、RGB
+        int vlcc = 5;//色を変える速度　1か5か255のみ
+        int score = 0;
 
         public Form1()
         {
             InitializeComponent();
+            //Form1.DefaultBackColor=Color.FromArgb(100,100,100);
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -27,26 +31,36 @@ namespace hukusyu0531
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            Text = MousePosition.X + ", " + MousePosition.Y;
+            Point p = PointToClient(MousePosition);
+
+            label2.Left = p.X - label2.Width / 2;
+            label2.Top = p.Y - label2.Height / 2;
+
             label1.Left += vx;
             label1.Top += vy;
 
+            label3.Left = (ClientSize.Width) - (label3.Width);
+            label3.Text = ("SCORE " + score);
+
             if(label1.Left <= 0)
             {
-                vx = -vx;
+                vx = Math.Abs(vx);
             }
             if (label1.Top <= 0)
             {
-                vy = -vy;
+                vy = Math.Abs(vy);
             }
-            if (label1.Left >= (Width)-50)
+            if (label1.Left >= (ClientSize.Width)-(label1.Width))
             {
-                vx = -vx;
+                vx = -Math.Abs(vx);
             }
-            if (label1.Top >= (Height)-50)
+            if (label1.Top >= (ClientSize.Height)-(label1.Height))
             {
-                vy = -vy;
+                vy = -Math.Abs(vy);
             }
 
+            //label1の動きの向きになる矢印
             if (vx<0&&vy<0)
             {
                 label1.Text = ("↖");
@@ -63,6 +77,64 @@ namespace hukusyu0531
             {
                 label1.Text = ("↘");
             }
+
+            //label1が虹色
+            label1.ForeColor = Color.FromArgb(lcr, lcg, lcb);
+            if (lcr == 255 && lcg < 255 && lcb == 0)
+            {
+                lcg += vlcc;//赤～黄色でGを増やす
+            }
+            if (lcr > 0 && lcg == 255 && lcb == 0)
+            {
+                lcr -= vlcc;//黄色～緑でRを減らす
+            }
+            if (lcr == 0 && lcg == 255 && lcb < 255)
+            {
+                lcb += vlcc;//緑～水色でBを増やす
+            }
+            if (lcr == 0 && lcg > 0 && lcb == 255)
+            {
+                lcg -= vlcc;//水色～青でGを減らす
+            }
+            if (lcr < 255 && lcg == 0 && lcb == 255)
+            {
+                lcr += vlcc;//青～紫でRを増やす
+            }
+            if (lcr == 255 && lcg == 0 && lcb > 0)
+            {
+                lcb -= vlcc;//紫～赤でBを減らす
+            }
+
+            if (p.X >= label1.Left && p.X <= (label1.Left+label1.Width) && p.Y >= label1.Top && p.Y <= (label1.Top+label1.Height))
+            {
+                //label2.Visible=false;
+                label2.Text = ("(´∀`)");
+                label2.ForeColor = Color.FromArgb(255, 255, 255);
+                score = score + 100;
+            }
+            else
+            {
+                //label2.Visible = true;
+                label2.Text = ("(´Д`)");
+                label2.ForeColor = Color.FromArgb(0, 0, 0);
+                score = score;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("" + ClientSize.Width + ", " + ClientSize.Height);
+            MessageBox.Show("" + label1.Width + ", " + label1.Height);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
